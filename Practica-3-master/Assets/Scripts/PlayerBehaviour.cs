718bbaa98903279208ba36b80c6c9b17486723a0
ibaibaglietto <ibaibaglietto@gmail.com>
2019-02-28 11:48:46 +0100

    Scripts player y skeleton

--- Practica-3-master/Assets/Scripts/PlayerBehaviour.cs
+++ Practica-3-master/Assets/Scripts/PlayerBehaviour.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-	// Definir Hashes de:
-	// Parametros (Speed, Attack, Damage, Dead)
-	// Estados (Base Layer.Idle, Attack Layer.Idle, Attack Layer.Attack)
-	// TODO
+    // Definir Hashes de:
+    // Parametros (Speed, Attack, Damage, Dead)
+    int speedHash = Animator.StringToHash("Speed");
+    int attackHash = Animator.StringToHash("Attack");
+    int damageHash = Animator.StringToHash("Damage");
+    int deadHash = Animator.StringToHash("Dead");
+    // Estados (Base Layer.Idle, Attack Layer.Idle, Attack Layer.Attack)
+    int idleBaseHash = Animator.StringToHash("Base Layer.Idle");
+    int idleAttackHash = Animator.StringToHash("Attack Layer.Idle");
+    int attackAttackHash = Animator.StringToHash("Attack Layer.Attack");
+    // TODO
+    Animator anim;
+    Rigidbody rb;
 
 	public float walkSpeed		= 1;		// Parametro que define la velocidad de "caminar"
 	public float runSpeed		= 1;		// Parametro que define la velocidad de "correr"
@@ -24,8 +33,11 @@ public class PlayerBehaviour : MonoBehaviour
 
 	void Start()
 	{
-		// Obtener los componentes Animator, Rigidbody y el valor original center.z del BoxCollider
-		// TODO
+        // Obtener los componentes Animator, Rigidbody y el valor original center.z del BoxCollider
+        // TODO
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        _originalColliderZ = GetComponent<BoxCollider>().center.z;
 	}
 
 	// Aqui moveremos y giraremos la araña en funcion del Input
@@ -37,77 +49,133 @@ public class PlayerBehaviour : MonoBehaviour
 		// Calculo de velocidad lineal (_speed) y angular (_angularSpeed) en función del Input
 		// Si camino/corro hacia delante delante: _speed = walkSpeed   /  _speed = runSpeed
 		// TODO
-
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (Input.GetKey(KeyCode.LeftShift)){
+                _speed = runSpeed;
+            }
+            else
+            {
+                _speed = walkSpeed;
+            }
+        }
 		// Si camino/corro hacia delante detras: _speed = -walkSpeed   /  _speed = -runSpeed
 		// TODO
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _speed = -walkSpeed;
+        }
 
-		// Si no me muevo: _speed = 0
-		// TODO
 
-		// Si giro izquierda: _angularSpeed = -rotateSpeed;
-		// TODO
+        // Si no me muevo: _speed = 0
+        // TODO
+        else
+        {
+            _speed = 0;
+        }
+        // Si giro izquierda: _angularSpeed = -rotateSpeed;
+        // TODO
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _angularSpeed = -rotateSpeed;
+        }
 
-		// Si giro derecha: _angularSpeed = rotateSpeed;
-		// TODO
 
-		// Si no giro : _angularSpeed = 0;
-		// TODO
-
-		// Actualizamos el parámetro "Speed" en función de _speed. Para activar la anicación de caminar/correr
-		// TODO
-
-		// Movemov y rotamos el rigidbody (MovePosition y MoveRotation) en función de "_speed" y "_angularSpeed"
-		// TODO
-
-		// Mover el collider en función del parámetro "Distance" (necesario cuando atacamos)
-		// TODO
-	}
+        // Si giro derecha: _angularSpeed = rotateSpeed;
+        // TODO
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _angularSpeed = rotateSpeed;
+        }
+        // Si no giro : _angularSpeed = 0;
+        // TODO
+        else
+        {
+            _angularSpeed = 0;
+        }
+        // Actualizamos el parámetro "Speed" en función de _speed. Para activar la anicación de caminar/correr
+        // TODO
+        anim.SetFloat(speedHash, _speed);
+        // Movemov y rotamos el rigidbody (MovePosition y MoveRotation) en función de "_speed" y "_angularSpeed"
+        // TODO
+        rb.MovePosition(transform.position + transform.forward * _speed * 0.025f);
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(transform.up * _angularSpeed));
+        // Mover el collider en función del parámetro "Distance" (necesario cuando atacamos)
+        // TODO
+        BoxCollider bc = GetComponent<BoxCollider>();
+        bc.center = new Vector3(bc.center.x, bc.center.y, anim.GetFloat("Distance") * _originalColliderZ * 20);
+    }
 
 	// En este bucle solamente comprobaremos si el Input nos indica "atacar" y activaremos el trigger "Attack"
 	private void Update()
 	{
-		// Si estoy en pausa no hacer nada (no moverme ni atacar)
-		// TODO
 
-		// Si detecto Input tecla/boton ataque ==> Activo disparados 'Attack'
-	}
+        // Si estoy en pausa no hacer nada (no moverme ni atacar)
+        // TODO
+        if (paused) return;
+
+        // Si detecto Input tecla/boton ataque ==> Activo disparados 'Attack'
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(attackHash);
+        }
+
+    }
 
 	// Función para resetear el Player
 	public void reset()
 	{
-		//Reiniciar el numero de vidas
-		// TODO
-
-		// Pausamos a Player
-		// TODO
-
-		// Forzar estado Idle en las dos capas (Base Layer y Attack Layer): función Play() de Animator
-		// TODO
-
-		// Reseteo todos los triggers (Attack y Dead)
-		// TODO
-
-		// Posicionar el jugador en el (0,0,0) y rotación nula (Quaternion.identity)
-		// TODO
+        //Reiniciar el numero de vidas
+        // TODO
+        _lives = 3;
+        // Pausamos a Player
+        // TODO
+        paused = true;
+        // Forzar estado Idle en las dos capas (Base Layer y Attack Layer): función Play() de Animator
+        // TODO
+        anim.Play(idleBaseHash);
+        anim.Play(idleAttackHash);
+        // Reseteo todos los triggers (Attack y Dead)
+        // TODO
+        anim.ResetTrigger(attackHash);
+        anim.ResetTrigger(deadHash);
+        // Posicionar el jugador en el (0,0,0) y rotación nula (Quaternion.identity)
+        // TODO
+        rb.MovePosition(Vector3.zero);
+        rb.MoveRotation(Quaternion.identity);
 	}
 
 	// Funcion recibir daño
 	public void recieveDamage()
 	{
-		// Restar una vida
-		// Si no me quedan vidas notificar al GameManager (notifyPlayerDead) y disparar trigger "Dead"
-		// TODO
+        // Restar una vida
+        // Si no me quedan vidas notificar al GameManager (notifyPlayerDead) y disparar trigger "Dead"
+        // TODO
+        _lives -= 1;
+        if (_lives == 0)
+        {
+            //GameManager.notifyPlayerDead();
+            anim.SetTrigger(deadHash);
 
-		// Si aun me quedan vidas dispara el trigger TakeDamage
-		// TODO
+        }
+        // Si aun me quedan vidas dispara el trigger TakeDamage
+        // TODO
+        else
+        {
+            anim.SetTrigger(damageHash);
+        }
 	}
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		// Obtener estado actual de la capa Attack Layer
-		// TODO
-
-		// Si el estado es 'Attack' matamos al enemigo (mirar etiqueta)
-		// TODO
-	}
+        // Obtener estado actual de la capa Attack Layer
+        // TODO
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(1);
+        // Si el estado es 'Attack' matamos al enemigo (mirar etiqueta)
+        // TODO
+        if (stateInfo.fullPathHash == attackAttackHash && collision.gameObject.tag == "Enemy" && anim.GetFloat("Distance")>0.0f)
+        {
+            collision.gameObject.GetComponent<SkeletonBehaviour>().kill();
+        }
+    }
 }
--- Practica-3-master/Assets/Scripts/SkeletonBehaviour.cs
+++ Practica-3-master/Assets/Scripts/SkeletonBehaviour.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class SkeletonBehaviour : MonoBehaviour
 {
-	// Definir Hashes de:
-	// Parametros (Attack, Dead, Distance)
-	// Estados (Attack, Idle)
-	// TODO
+    // Definir Hashes de:
+    // Parametros (Attack, Dead, Distance)
+    int attackHash = Animator.StringToHash("Attack");
+    int damageHash = Animator.StringToHash("Damage");
+    int deadHash = Animator.StringToHash("Dead");
+    // Estados (Attack, Idle)
+    int idleStateHash = Animator.StringToHash("Base Layer.Idle");
+    int attackStateHash = Animator.StringToHash("Base Layer.Attack");
+    // TODO
 
-	// Variables auxiliares 
-	PlayerBehaviour _player		= null;     //Puntero a Player (establecido por método 'setPlayer')
+    Animator anim;
+
+    // Variables auxiliares 
+    PlayerBehaviour _player		= null;     //Puntero a Player (establecido por método 'setPlayer')
 	bool _dead					= false;	// Indica si ya he sido eliminado
 	float _originalColliderZ	= 0;        // Valora original de la posición 'z' del collider
 	float _timeToAttack			= 0;		// Periodo de ataque
@@ -22,52 +29,79 @@ public class SkeletonBehaviour : MonoBehaviour
 
 	void Start ()
 	{
-		// Obtener los componentes Animator y el valor original center.z del BoxCollider
-		// TODO
-	}
+        // Obtener los componentes Animator y el valor original center.z del BoxCollider
+        // TODO
+        anim = GetComponent<Animator>();
+        _originalColliderZ = GetComponent<BoxCollider>().center.z;
+    }
 	
 	void FixedUpdate ()
 	{
-		// Si estoy muerto ==> No hago nada
-		// TODO
+        
+        // Si estoy muerto ==> No hago nada
+        // TODO
+        if (_dead) return;
+        // Si Player esta a menos de 1m de mi y no estoy muerto:
+        // - Le miro
+        // - Si ha pasado 1s o más desde el ultimo ataque ==> attack()
+        // TODO
+        _timeToAttack += Time.fixedDeltaTime;
+        //float distPlayer = Mathf.Sqrt(Mathf.Pow(transform.position.x - _player.transform.position.x, 2) + Mathf.Pow(transform.position.z - _player.transform.position.z, 2));
+        //if (distPlayer < 1.0f)
+        //{
+            //transform.LookAt(_player.transform.position);
+            if (_timeToAttack > 1.0f)
+            {
+                attack();
+                _timeToAttack = 0.0f;
+            }
+        //}
+        // Desplazar el collider en 'z' un multiplo del parametro Distance
+        // TODO
+        BoxCollider bc = GetComponent<BoxCollider>();
+        bc.center = new Vector3(bc.center.x, bc.center.y, anim.GetFloat("Distance") * _originalColliderZ * 40);
 
-		// Si Player esta a menos de 1m de mi y no estoy muerto:
-		// - Le miro
-		// - Si ha pasado 1s o más desde el ultimo ataque ==> attack()
-		// TODO
+    }
 
-		// Desplazar el collider en 'z' un multiplo del parametro Distance
-		// TODO
-	}
-
-	public void attack()
+    public void attack()
 	{
-		// Activo el trigger "Attack"
-		// TODO
+        // Activo el trigger "Attack"
+        // TODO
+        anim.SetTrigger(attackHash);
 	}
 
 	public void kill()
 	{
-		// Guardo que estoy muerto, disparo trigger "Dead" y desactivo el collider
-		// TODO
+        // Guardo que estoy muerto, disparo trigger "Dead" y desactivo el collider
+        // TODO
+        _dead = true;
+        anim.SetTrigger(deadHash);
+        GetComponent<BoxCollider>().enabled = false;
+        // Notifico al GameManager que he sido eliminado
+        // TODO
+        //GameManager.notifyEnemyKilled(gameObject);
+    }
 
-		// Notifico al GameManager que he sido eliminado
-		// TODO
-	}
-
-	// Funcion para resetear el collider (activado por defecto), la variable donde almaceno si he muerto y forzar el estado "Idle" en Animator
-	public void reset()
+    // Funcion para resetear el collider (activado por defecto), la variable donde almaceno si he muerto y forzar el estado "Idle" en Animator
+    public void reset()
 	{
-		// TODO
-	}
+        GetComponent<BoxCollider>().enabled = true;
+        _dead = true;
+        anim.ResetTrigger(deadHash);
+        anim.Play(idleStateHash);
+    }
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		// Obtener el estado actual
-		// TODO
-
-		// Si el estado es 'Attack' y el parametro Distance es > 0 atacamos a Player (comprobar etiqueta).
-		// La Distancia >0 es para acotar el ataque sólo al momento que mueve la espada (no toda la animación).
-		// TODO
-	}
+        // Obtener el estado actual
+        // TODO
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        // Si el estado es 'Attack' y el parametro Distance es > 0 atacamos a Player (comprobar etiqueta).
+        // La Distancia >0 es para acotar el ataque sólo al momento que mueve la espada (no toda la animación).
+        // TODO
+        if (stateInfo.fullPathHash == attackStateHash && collision.gameObject.tag == "Player" && anim.GetFloat("Distance") > 0.0f)
+        {
+            collision.gameObject.GetComponent<PlayerBehaviour>().recieveDamage();
+        }
+    }
 }
